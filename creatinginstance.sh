81e2e78455b0d1a566a62c64973f399ea343e11e
Ajay <ajaylearner01@gmail.com>
2025-05-20 12:06:25 +0530

    clas16

--- creatinginstance.sh
+++ creatinginstance.sh
@@ -8,8 +8,7 @@ DOMAIN_NAME="ajay6.space"
 
 for instance in ${INSTANCES[@]}
  do 
-  INSTANCEID=${aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0a7153f8526f29a8e \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text}
+  INSTANCEID=${aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0a7153f8526f29a8e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=test}]" --query "Instances[0].InstanceId" --output text}
    if [ $instance != frontend ]
    then
      IP=${aws ec2 describe-instances --instance-ids $INSTANCEID --query "Instances[0].PrivateIpAddress" --output text}
