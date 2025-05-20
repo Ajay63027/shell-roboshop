ac95180c410bc4ac4d15268c2298a28bf9c70aa9
Ajay <ajaylearner01@gmail.com>
2025-05-20 19:05:53 +0530

    class17

--- catalogue.sh
+++ catalogue.sh
@@ -85,7 +85,7 @@ dnf install mongodb-mongosh -y &>>$logfile
 VALIDATE $? "installing mongoclient"
 
 STATUS=$(mongosh --host mongodb.ajay6.space --eval 'db.getMongo().getDBNames().indexOf("catalogue")' --quiet)
-if [ STATUS -lt 0 ];then
+if [ $STATUS -lt 0 ];then
 mongosh --host mongodb.ajay6.space </app/db/master-data.js &>>$logfile
 else
 echo -e "already data existed $Y SKIP THIS PART $N"
