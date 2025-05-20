39124908b224f1510ee68b6fadde01b35acf444a
Ajay <ajaylearner01@gmail.com>
2025-05-20 15:52:51 +0530

    class17

--- route53.sh
+++ route53.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash 
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0a7153f8526f29a8e"
@@ -11,11 +11,11 @@ for instance in ${INSTANCES[@]}
   INSTANCEID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0a7153f8526f29a8e --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
    if [ $instance != frontend ]
    then
-     IP=$(aws ec2 describe-instances --instance-ids $INSTANCEID --query "Instances[0].PrivateIpAddress" --output text)
+     IP=$(aws ec2 describe-instances --instance-ids $INSTANCEID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
    else
-     IP=$(aws ec2 describe-instances --instance-ids $INSTANCEID --query "Instances[0].PublicIpAddress" --output text)
-     echo="$instance ID IP address: $IP"
+     IP=$(aws ec2 describe-instances --instance-ids $INSTANCEID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
    fi
+   echo "$instance ID IP address: $IP"
 
 aws route53 change-resource-record-sets \
   --hosted-zone-id $ZONE_ID \
